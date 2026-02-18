0f1b2999288ec27f07f505d11e2bc73c6b8d9c9e
Dainy Jose <dainyjose.mary@gmail.com>
2026-02-18 23:28:09 +0530

    feat(QRGenerator): add QR code generation and image download to gallery feature

--- App.tsx
+++ App.tsx
@@ -1,11 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useRef, useState } from "react";
+import { Alert, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import QRCode from "react-native-qrcode-svg";
+import { captureRef } from "react-native-view-shot";
+import * as MediaLibrary from "expo-media-library";
+import * as FileSystem from "expo-file-system/legacy";
 
 export default function App() {
+  const [status, requestPermission] = MediaLibrary.usePermissions();
+  const viewRef = useRef(null);
+  const [saving, setSaving] = useState(false);
+  let url = "https://dainyjose.github.io/my-portfolio/";
+
+  const downloadQrCode = async () => {
+    try {
+      if (!status?.granted) {
+        const { granted } = await requestPermission();
+        if (!granted) {
+          Alert.alert(
+            "Permission denied",
+            "Cannot save without gallery access."
+          );
+          return;
+        }
+      }
+
+      setSaving(true);
+      const uri = await captureRef(viewRef, {
+        format: "png",
+        quality: 1,
+        result: "tmpfile",
+        scale: 3,
+      });
+      const filePath = `${FileSystem.cacheDirectory}QR_code.png`;
+
+      await FileSystem.copyAsync({
+        from: uri,
+        to: filePath,
+      });
+
+      const asset = await MediaLibrary.createAssetAsync(filePath);
+      await MediaLibrary.createAlbumAsync("QR Codes", asset, false);
+
+      Alert.alert("Success", "QR Code saved to gallery!");
+    } catch (error) {
+      console.error("Save failed:", error);
+      Alert.alert("Error", "Failed to save QR code");
+    } finally {
+      setSaving(false);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View
+        ref={viewRef}
+        collapsable={false}
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "white",
+          padding: 20,
+        }}
+      >
+        <QRCode
+          value={url}
+          size={220}
+          // logo={require("./assets/icon.png")}
+          logoSize={40}
+          logoMargin={1}
+          logoBorderRadius={8}
+          backgroundColor="white"
+        />
+      </View>
+      <TouchableOpacity
+        style={{
+          marginBottom: 20,
+          backgroundColor: "lightgray",
+          paddingHorizontal: 20,
+          paddingVertical: 10,
+          borderRadius: 20,
+          top: 20,
+        }}
+        onPress={downloadQrCode}
+      >
+        <Text>{saving ? "Saving..." : "Save QR to Gallery"}</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +92,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ README.md
@@ -0,0 +1,42 @@
+<div align="center">
+
+  <h1>QR Code Generator & Downloader</h1>
+  <p>A simple, fast, and customizable QR code generator app with image download support.</p>
+
+</div>
+
+---
+
+## Overview
+
+**QR Code Generator & Downloader** is a cross-platform mobile application built with **React Native** and **Expo**.  
+It allows users to **generate customizable QR codes** from text or URLs and **download them as images** directly to their gallery.
+
+---
+
+## Features
+
+- Generate QR codes instantly from text or URLs
+<!-- - Customize color and background of QR codes   -->
+- Download and save generated QR codes as images
+  <!-- - Share QR codes via other apps   -->
+  <!-- - Fast, lightweight, and user-friendly interface   -->
+
+---
+
+## Tech Stack
+
+- **React Native / Expo** – cross-platform mobile app framework
+- **react-native-qrcode-svg** – for generating QR codes
+- **react-native-view-shot** – for capturing views as images
+- **react-native-fs** – for saving files to local storage
+
+---
+
+## Installation & Setup
+
+1. **Clone the repository**
+   ```bash
+   git clone https://github.com/dainyjose/qr-code-generator.git
+   cd qr-code-generator
+   ```
